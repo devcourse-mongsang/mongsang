c0f6482ca8521d231a62747fcec026096491dc5f
Seungyeon Han <tiffanyhansy@gmail.com>
2025-01-18 23:38:21 +0900

    ✨ feat: Button 컴포넌트 구현

---
+++ src/components/common/Button.vue
@@ -0,0 +1,59 @@
+<template>
+  <button
+    :class="[
+      'focus:outline-none flex items-center justify-center',
+      buttonVar[variant],
+      buttonSize[size],
+      className,
+    ]"
+    v-bind="otherProps"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Button",
+  props: {
+    variant: {
+      type: String,
+      required: true,
+      validator: (value) =>
+        ["filled", "outlined", "shadowed", "custom"].includes(value),
+    },
+    size: {
+      type: String,
+      required: true,
+      validator: (value) => ["sm", "md", "lg"].includes(value),
+    },
+
+    className: {
+      type: String,
+      default: "",
+    },
+  },
+  setup(props, { attrs }) {
+    const buttonVar = {
+      filled: "bg-hc-blue text-hc-white",
+      regular: "text-hc-blue bg-hc-white",
+      shadowed: "shadow-blue bg-hc-blue text-hc-white",
+      custom: "",
+    };
+
+    const buttonSize = {
+      sm: "w-[50px] h-[50px] rounded-[70px]",
+      md: "w-[160px] h-[48px] text-[16px] rounded-[20px]",
+      lg: "w-[480px] h-[63px] text-[24px] rounded-[70px] font-semibold",
+    };
+
+    return {
+      buttonVar,
+      buttonSize,
+      otherProps: attrs,
+    };
+  },
+});
+</script>
