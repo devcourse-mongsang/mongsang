61a5647e4480aad6bb9d75261318300068e34637
최민혁 <130155327+lactofreee@users.noreply.github.com>
2025-01-21 01:10:39 +0900

    ✨ Feat: 커뮤니티 게시물 상세페이지 반응형 UI 퍼블리싱

--- src/pages/community-pages/CommunityBoard.vue
+++ src/pages/community-pages/CommunityBoard.vue
@@ -42,27 +42,29 @@ watch(
     <ul v-if="currentBoard.posts && currentBoard.posts.length">
       <li v-for="post in currentBoard.posts" :key="post.id">
         <RouterLink :to="`/${route.params.boardType}/${post.id}`" class="mb-7">
-          <div class="flex justify-between mx-4 mb-7">
-            <div class="flex flex-col mt-6 gap-7">
+          <div class="flex items-center justify-between mx-4 mb-7">
+            <div class="flex flex-col xm:mt-0 sm:mt-6 sm:gap-7 xm:gap-6">
               <span class="flex items-center gap-[10px]">
                 <img
-                  class="h-[50px] w-[50px] rounded-full"
+                  class="aspect-square xm:w-[30px] sm:w-[50px] rounded-full"
                   :src="post.profile_img || imgPlaceholder"
                   alt="User profile image"
                 />
-                <p class="font-semibold lg:text-xl">{{ post.userName }}</p>
+                <p class="font-semibold lg:text-xl">
+                  {{ post.userName || "@anonymous" }}
+                </p>
               </span>
               <span class="flex flex-col">
-                <h2 class="text-xl font-semibold lg:text-2xl">
+                <h2 class="font-semibold sm:text-xl lg:text-2xl">
                   {{ post.title }}
                 </h2>
-                <p class="text-[#757575]">
+                <p class="text-[#757575] xm:text-xs sm:text-base">
                   {{ dateConverter(post.created_at) }}
                 </p>
               </span>
             </div>
             <img
-              class="w-[180px] h-[180px] rounded-[20px]"
+              class="sm:w-[180px] sm:h-[180px] w-[100px] h-[100px] rounded-[20px]"
               :src="post.img_url || imgPlaceholder"
               alt="Post image"
             />
