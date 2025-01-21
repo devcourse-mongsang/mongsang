884cdf98155229247aca0e8d4e9fc3cd36d4245a
Seungyeon Han <tiffanyhansy@gmail.com>
2025-01-21 12:46:07 +0900

    ✨ feat: Vuetify 설치

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createPinia } from "pinia";
 import router from "./router/index";
 import "./assets/styles/index.css";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 import { createApp } from "vue";
 import App from "./App.vue";
@@ -8,6 +12,12 @@ import App from "./App.vue";
 const pinia = createPinia();
 const app = createApp(App);
 
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
 app.use(router);
 app.use(pinia);
+app.use(vuetify);
 app.mount("#app");
