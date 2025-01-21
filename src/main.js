0c08e8e6df3ea1cf4190e512df4af754ea0718dd
Seungyeon Han <tiffanyhansy@gmail.com>
2025-01-21 15:24:47 +0900

    🐛 fix: vuetify 이중 선언 오류 해결

--- src/main.js
+++ src/main.js
@@ -8,10 +8,6 @@ import App from "./App.vue";
 
 import "vuetify/styles";
 import "./assets/styles/index.css";
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
 
 const vuetify = createVuetify({
   components,
@@ -20,11 +16,6 @@ const vuetify = createVuetify({
 
 const pinia = createPinia();
 const app = createApp(App);
-
-const vuetify = createVuetify({
-  components,
-  directives,
-});
 
 app.use(router);
 app.use(pinia);
