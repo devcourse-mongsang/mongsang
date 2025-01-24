6aede59192ebe30274ace8f3fd02b39e8911ee15
Seungyeon Han <tiffanyhansy@gmail.com>
2025-01-25 02:56:33 +0900

    ✨ feat: 꿈 기록 페이지와 일기장 작성 페이지의 데이터 연결

--- src/pages/diary-pages/DiaryWrite.vue
+++ src/pages/diary-pages/DiaryWrite.vue
@@ -3,21 +3,23 @@ import { ref, computed, onMounted } from "vue";
 import DropDownWeather from "@/components/common/DropDownWeather.vue";
 import DropDownFace from "../../components/common/DropDownFace.vue";
 import { Icon } from "@iconify/vue";
+import { useDiaryStore } from "@/store/diaryStore";
 
 // 상태 관리
 const selectedDate = ref(new Date());
+const diaryStore = useDiaryStore();
 
 // 더미 데이터
 const diaryData = ref({
   date: "2025년 / 01월 / 20일",
   username: "@Mala_love",
   title: "",
-  content:
-    "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
-  dreamAnalysis:
-    "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
-  aiImg: "/assets/imgs/img_placeholder.png",
-  asmrImg: "/assets/imgs/youtube_thumbnail.png",
+  // content:
+  //   "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+  // dreamAnalysis:
+  //   "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+  // aiImg: "/assets/imgs/img_placeholder.png",
+  // asmrImg: "/assets/imgs/youtube_thumbnail.png",
 });
 
 // 제목과 본문 초기화
@@ -53,19 +55,18 @@ const hasContent = ref(false); // 버튼 표시 여부
 // content 확인
 const checkContent = () => {
   hasContent.value =
-    content.value.trim().length > 0 ||
-    diaryData.value.content.trim().length > 0;
+    content.value.trim().length > 0 || diaryStore.content.trim().length > 0;
 };
 
 // 버튼 클릭 동작
 const fetchContent = () => {
-  content.value = diaryData.value.content; // diaryData의 content를 textarea로 복사
+  content.value = diaryStore.content; // diaryData의 content를 textarea로 복사
   checkContent(); // 버튼 상태 업데이트
 };
 
 // 초기화 시 diaryData의 content를 content에 복사
 onMounted(() => {
-  content.value = diaryData.content;
+  content.value = diaryStore.content;
 });
 
 // 데이터 표시 상태 관리
@@ -88,7 +89,7 @@ const toggleAsmrImage = () => {
 </script>
 
 <template>
-  <div class="flex justify-center items-start p-4 xm:p-0">
+  <div class="flex items-start justify-center p-4 xm:p-0">
     <div class="w-full max-w-[710px] xm:w-full">
       <!-- 날짜 표시 및 상단 버튼들 -->
       <div class="flex justify-between gap-4 mb-5 xm:mb-0">
@@ -108,7 +109,7 @@ const toggleAsmrImage = () => {
         </div>
 
         <!-- 상단 우측 버튼들 -->
-        <div class="flex gap-4 justify-end ml-auto">
+        <div class="flex justify-end gap-4 ml-auto">
           <DropDownFace />
           <DropDownWeather />
         </div>
@@ -125,7 +126,7 @@ const toggleAsmrImage = () => {
           <input
             v-model="title"
             placeholder="제목을 입력해주세요"
-            class="w-full text-lg sm:text-2xl font-semibold text-hc-gray mb-4 sm:mb-2 bg-transparent border-none focus:outline-none"
+            class="w-full mb-4 text-lg font-semibold bg-transparent border-none sm:text-2xl text-hc-gray sm:mb-2 focus:outline-none"
           />
           <div class="relative">
             <!-- content textarea -->
@@ -142,7 +143,7 @@ const toggleAsmrImage = () => {
             >
               <Icon
                 icon="material-symbols:edit-note-rounded"
-                class="w-6 h-6 absolute text-hc-blue top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                class="absolute w-6 h-6 transform -translate-x-1/2 -translate-y-1/2 text-hc-blue top-1/2 left-1/2"
               />
             </button>
           </div>
@@ -150,35 +151,35 @@ const toggleAsmrImage = () => {
 
         <!-- 미리보기 섹션 -->
         <div class="mt-6 xm:mt-11 xm:px-6">
-          <p class="text-lg sm:text-xl font-semibold mb-2">미리보기</p>
+          <p class="mb-2 text-lg font-semibold sm:text-xl">미리보기</p>
           <div class="space-y-4">
             <!-- 꿈 분석 -->
             <div class="relative w-[620px]">
               <p
-                class="text-lg sm:text-xl font-semibold text-hc-blue mb-2 inline-block"
+                class="inline-block mb-2 text-lg font-semibold sm:text-xl text-hc-blue"
               >
                 꿈 분석
               </p>
               <button
-                v-if="diaryData.dreamAnalysis"
+                v-if="diaryStore.dreamAnalysis"
                 @click="deleteDreamAnalysis"
-                class="absolute right-0 top-0"
+                class="absolute top-0 right-0"
               >
                 <div
                   class="flex justify-start items-center w-[30px] h-[30px] relative gap-2.5 p-[3px] rounded-[15px] bg-[#757575]"
                 >
                   <Icon
                     icon="mdi:trash-can-outline"
-                    class="w-5 h-5 text-hc-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                    class="absolute w-5 h-5 transform -translate-x-1/2 -translate-y-1/2 text-hc-white top-1/2 left-1/2"
                   />
                 </div>
               </button>
               <div
                 v-if="showDreamAnalysis"
-                class="h-20 sm:h-32 overflow-y-auto"
+                class="h-20 overflow-y-auto sm:h-32"
               >
                 <p class="text-base sm:text-xl text-hc-black">
-                  {{ diaryData.dreamAnalysis }}
+                  {{ diaryStore.dreamAnalysis }}
                 </p>
               </div>
               <div
@@ -191,7 +192,7 @@ const toggleAsmrImage = () => {
                 >
                   <Icon
                     icon="material-symbols:notes-rounded"
-                    class="w-6 h-6 absolute text-hc-blue top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                    class="absolute w-6 h-6 transform -translate-x-1/2 -translate-y-1/2 text-hc-blue top-1/2 left-1/2"
                   />
                 </button>
               </div>
@@ -200,27 +201,27 @@ const toggleAsmrImage = () => {
             <!-- 이미지와 ASMR -->
             <div class="flex flex-wrap gap-4">
               <div class="relative">
-                <p class="text-lg sm:text-xl font-semibold text-hc-blue mb-2">
+                <p class="mb-2 text-lg font-semibold sm:text-xl text-hc-blue">
                   이미지
                 </p>
                 <div class="relative">
                   <button
-                    v-if="diaryData.aiImg"
+                    v-if="diaryStore.imgUrl"
                     @click="deleteImage"
-                    class="absolute -top-2 -right-2 z-10"
+                    class="absolute z-10 -top-2 -right-2"
                   >
                     <div
                       class="flex justify-start items-center w-[30px] h-[30px] relative gap-2.5 p-[3px] rounded-[15px] bg-[#757575]"
                     >
                       <Icon
                         icon="mdi:trash-can-outline"
-                        class="w-5 h-5 text-hc-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                        class="absolute w-5 h-5 transform -translate-x-1/2 -translate-y-1/2 text-hc-white top-1/2 left-1/2"
                       />
                     </div>
                   </button>
                   <img
                     v-if="showAiImage"
-                    :src="diaryData.aiImg"
+                    :src="diaryStore.imgUrl"
                     class="w-20 h-20 sm:w-[90px] sm:h-[90px] object-cover rounded-[20px]"
                     alt="AI Image"
                   />
@@ -229,43 +230,46 @@ const toggleAsmrImage = () => {
                     class="w-20 h-20 sm:w-[90px] sm:h-[90px] bg-[#D9D9D9] rounded-[20px] relative"
                   >
                     <button
-                      v-if="diaryData.aiImg"
+                      v-if="diaryStore.imgUrl"
                       @click="toggleAiImage"
                       class="w-[50px] h-[50px] rounded-full bg-hc-white/30 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
                     >
                       <Icon
                         icon="material-symbols:photo-library-rounded"
-                        class="w-6 h-6 absolute text-hc-blue top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                        class="absolute w-6 h-6 transform -translate-x-1/2 -translate-y-1/2 text-hc-blue top-1/2 left-1/2"
                       />
                     </button>
                   </div>
                 </div>
               </div>
               <div class="relative">
-                <p class="text-lg sm:text-xl font-semibold text-hc-blue mb-2">
+                <p class="mb-2 text-lg font-semibold sm:text-xl text-hc-blue">
                   ASMR
                 </p>
                 <div class="relative">
                   <button
-                    v-if="diaryData.asmrImg"
+                    v-if="diaryStore.youtubeUrl"
                     @click="deleteAsmr"
-                    class="absolute -top-2 -right-2 z-10"
+                    class="absolute z-10 -top-2 -right-2"
                   >
                     <div
                       class="flex justify-start items-center w-[30px] h-[30px] relative gap-2.5 p-[3px] rounded-[15px] bg-[#757575]"
                     >
                       <Icon
                         icon="mdi:trash-can-outline"
-                        class="w-5 h-5 text-hc-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                        class="absolute w-5 h-5 transform -translate-x-1/2 -translate-y-1/2 text-hc-white top-1/2 left-1/2"
                       />
                     </div>
                   </button>
-                  <img
-                    v-if="showAsmrImage"
-                    :src="diaryData.asmrImg"
+                  <iframe
+                    v-if="diaryStore.youtubeUrl"
                     class="w-28 h-20 sm:w-[127px] sm:h-[90px] object-cover rounded-[20px]"
-                    alt="ASMR Image"
-                  />
+                    :src="diaryStore.youtubeUrl"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                  ></iframe>
+
                   <div
                     v-else
                     class="w-28 h-20 sm:w-[127px] sm:h-[90px] bg-[#D9D9D9] rounded-[20px] relative"
@@ -277,7 +281,7 @@ const toggleAsmrImage = () => {
                     >
                       <Icon
                         icon="material-symbols:slideshow-outline-rounded"
-                        class="w-6 h-6 absolute text-hc-blue top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                        class="absolute w-6 h-6 transform -translate-x-1/2 -translate-y-1/2 text-hc-blue top-1/2 left-1/2"
                       />
                     </button>
                   </div>
@@ -289,11 +293,11 @@ const toggleAsmrImage = () => {
       </div>
 
       <!-- 하단 동그라미 버튼 -->
-      <div class="mt-4 flex justify-end">
+      <div class="flex justify-end mt-4">
         <button class="w-[50px] h-[50px] bg-hc-blue rounded-full relative">
           <Icon
             icon="material-symbols:check-rounded"
-            class="w-6 h-6 text-hc-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            class="absolute w-6 h-6 transform -translate-x-1/2 -translate-y-1/2 text-hc-white top-1/2 left-1/2"
           />
         </button>
       </div>
@@ -301,6 +305,4 @@ const toggleAsmrImage = () => {
   </div>
 </template>
 
-<style scoped>
-/* 필요한 경우 여기에 추가 스타일을 넣을 수 있습니다 */
-</style>
+<style scoped></style>
