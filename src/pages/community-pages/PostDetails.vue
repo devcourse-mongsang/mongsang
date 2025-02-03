369557fad01d5ecfd6d9278f46790fd6167b8a84
최민혁 <130155327+lactofreee@users.noreply.github.com>
2025-02-03 21:44:48 +0900

    🐛 Fix: 커뮤니티 게시물 상세페이지 이미지 라운드 안들어가는 문제 해결

--- src/pages/community-pages/PostDetails.vue
+++ src/pages/community-pages/PostDetails.vue
@@ -234,26 +234,26 @@ register();
     <div>
       <swiper-container
         navigation="true"
-        class="mySwiper"
         initial-slide="0"
         pagination="true"
         centered-slides="true"
         slides-per-view="1"
         speed="450"
       >
-        <swiper-slide
-          v-for="(postImg, index) in postImgs"
-          :key="index"
-          class="md:rounded-[20px] bg-hc-white/50"
-        >
-          <div class="flex items-center aspect-square">
-            <img
-              class="object-contain w-full aspect-square"
-              :src="postImg"
-              alt="Post Image"
-            />
-          </div>
-        </swiper-slide>
+      <swiper-slide
+  v-for="(postImg, index) in postImgs"
+  :key="index"
+  class="md:rounded-[20px] bg-hc-white/50 overflow-hidden"
+>
+  <div class="flex items-center justify-center w-full h-full aspect-square">
+    <img
+      class="object-contain max-w-full max-h-full w-full h-full rounded-[20px]"
+      :src="postImg"
+      alt="Post Image"
+    />
+  </div>
+</swiper-slide>
+
       </swiper-container>
       <div class="mb-6">
         <div class="mt-[30px] px-4 sm:px-0 dark:text-hc-white">
